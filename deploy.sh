0071877894b085316715fcbdcc328fe1f71a5456
Egor Fedorov <egor.fedorov@codestetic.com>
2022-05-31 19:09:14 +0300

    Закончена разработка пайплайнов

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+docker login
 docker pull stalinon/orioksserver
+docker container rm -f orioksserver
 docker container run --name orioksserver -p 5000:5000 --network os_db_network --env-file server.env --rm -d stalinon/orioksserver
