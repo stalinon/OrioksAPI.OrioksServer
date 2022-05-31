cc63276f06b66029c4e4812e92b8bd3c3a217d29
Egor Fedorov <egor.fedorov@codestetic.com>
2022-05-31 15:41:54 +0300

    bash скрипт с версионированием и докеризацией приложения

---
+++ dockerize.sh
@@ -0,0 +1,20 @@
+#!/bin/bash  
+  
+image="$1/$3"
+login="$1"
+password="$2"
+
+echo "IMAGE_NAME = $image"
+timestamp=$(date +%Y%m%d%H%M%S)  
+  
+tag=$image:$timestamp  
+echo "TAG = $tag"
+
+latest=$image:latest
+
+docker build -f src/OrioksServer/Dockerfile -t $tag .  
+
+echo "$password" | docker login -u "$login" --password-stdin  
+docker push $image  
+
+docker system prune -f 
